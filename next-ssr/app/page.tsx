a67bf356f6c39661ce007eb954feeaf6fb03ab22
asm-dev <anasalamancamontero@gmail.com>
2025-04-11 20:22:26 +0100

    Improve error fallback adding a mock data retrieval option and update app page accordingly

--- next-ssr/app/page.tsx
+++ next-ssr/app/page.tsx
@@ -1,16 +1,53 @@
-import MovieList from "../../shared/components/MovieList";
-import { MoviesApiService } from "../../shared/services/movies/movies-api-service";
+"use client";
 
-export default async function HomePage() {
-  const apiKey = process.env.TMDB_API_KEY!;
-  const api = new MoviesApiService(apiKey);
+import { useEffect, useState } from "react";
+import ErrorFallback from "shared/components/ErrorFallback";
+import MovieList from "shared/components/MovieList";
+import { MOVIE_DATA_MOCK } from "shared/mocks/movie-data-mock";
+import { MoviesApiService } from "shared/services/movies/movies-api-service";
+import { MovieData } from "shared/services/movies/movies-api-service.types";
 
-  const movies = await api.getPopularMovies();
+export default function HomePage() {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [useMock, setUseMock] = useState(false);
+  const [retryKey, setRetryKey] = useState(0);
+
+  useEffect(() => {
+    if (useMock) {
+      setMovies(MOVIE_DATA_MOCK);
+      return;
+    }
+
+    const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY!;
+    const api = new MoviesApiService(apiKey);
+
+    api
+      .getPopularMovies()
+      .then((res) => setMovies(res.results))
+      .catch((err) => setError(err));
+  }, [useMock, retryKey]);
+
+  if (error) {
+    return (
+      <ErrorFallback
+        error={error}
+        reset={() => {
+          setError(null);
+          setRetryKey((key) => key + 1);
+        }}
+        useMock={() => {
+          setError(null);
+          setUseMock(true);
+        }}
+      />
+    );
+  }
 
   return (
     <main>
       <h1>Películas populares</h1>
-      <MovieList movieList={movies.results} />
+      <MovieList movieList={movies} />
     </main>
   );
 }
