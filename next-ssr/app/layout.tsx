d87e9492ea58aeb62bbba76f7f104fcf8bd596e2
asm-dev <anasalamancamontero@gmail.com>
2025-04-12 13:35:44 +0100

    Remove Chakra from shared and install on SSR directory

--- next-ssr/app/layout.tsx
+++ next-ssr/app/layout.tsx
@@ -1,33 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+"use client";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Prueba de Next.js",
-  description: "Proyecto de prueba enfocado a la implementación de enrrutado",
-};
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="es">
+      <body>
+        <ChakraProvider>{children}</ChakraProvider>
       </body>
     </html>
   );
