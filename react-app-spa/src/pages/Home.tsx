e562c601a740adb69f8bd3eb40ac1a23f5435903
asm-dev <anasalamancamontero@gmail.com>
2025-04-15 16:15:27 +0100

    Fix issue retlated to Vite env imports

--- react-app-spa/src/pages/Home.tsx
+++ react-app-spa/src/pages/Home.tsx
@@ -1,10 +1,9 @@
-// react-app-spa/src/pages/HomePage.tsx
 import { Link } from "react-router-dom";
 import Home from "shared/components/pages/Home";
 import SearchBarWithNavigate from "../components/SearchBarWithNavigate";
 
 export default function HomePage() {
-  const apiKey = process.env.REACT_APP_TMDB_API_KEY!;
+  const apiKey = import.meta.env.VITE_TMDB_API_KEY!;
 
   return (
     <Home
